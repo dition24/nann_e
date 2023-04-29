0239ee1cfaf5d5511ef07014bd342d7b086b0786
dition24 <117057559+dition24@users.noreply.github.com>
2023-04-29 00:21:10 -0400

    sets allowed host

--- nann_e/settings.py
+++ nann_e/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nann-e.herokuapp.com']
 
 
 # Application definition
